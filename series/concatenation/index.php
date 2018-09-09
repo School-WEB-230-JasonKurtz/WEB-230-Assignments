df3322b161e1bc3fa81e6fe10f245f91d214dd86
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-09-09 13:43:03 -0400

    Completed the concatenation assignment and added associate screen capture

---
+++ series/concatenation/index.php
@@ -0,0 +1,11 @@
+<?php
+    $day = 'Sunday';
+    $date = 9;
+    $year = 2018;
+
+    echo 'The date is <strong>'.$day.' '.$date.' '.$year.'</strong>.';
+
+    echo "<p>The date is $day $date $year.</p>";
+
+    echo '<p>The date is $day $date $year.</p>';
+?>
