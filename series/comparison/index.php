e5545fba33faaa3df86a98a6df7bb8cd27dedf53
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-09-13 21:18:10 -0400

    Added additional code to "comparison" assignment that was created during video

--- series/comparison/index.php
+++ series/comparison/index.php
@@ -6,4 +6,97 @@
     } else {
         echo '<p>Incorrect!</p>';
     }
+
+    if ($password=='password') {
+        echo '<p>Correct!</p>';
+    } else {
+        echo '<p>Incorrect!</p>';
+    }
+
+    if (true) {
+        echo '<p>True.</p>';
+    } else {
+        echo '<p>False.</p>';
+    }
+
+    if (false) {
+        echo '<p>True.</p>';
+    } else {
+        echo '<p>False.</p>';
+    }
+
+    if (1==1) {
+        echo '<p>True. 1==1</p>';
+    } else {
+        echo '<p>False. 1==1</p>';
+    }
+
+    if (1==2) {
+        echo '<p>True. 1==2</p>';
+    } else {
+        echo '<p>False. 1==2</p>';
+    }
+
+    if (1!=2) {
+        echo '<p>True. 1!=2</p>';
+    } else {
+        echo '<p>False. 1!=2</p>';
+    }
+
+    if (1!=1) {
+        echo '<p>True. 1!=1</p>';
+    } else {
+        echo '<p>False. 1!=1</p>';
+    }
+
+    if (1<>1) {
+        echo '<p>True. 1<>1</p>';
+    } else {
+        echo '<p>False. 1<>1</p>';
+    }
+
+
+    $number1 = 10;
+    $number2 = 9;
+
+    if ($number1>$number2) {
+        echo "<p>True. $number1>$number2</p>";
+    } else {
+        echo "<p>False. $number1>$number2</p>";
+    }
+
+    if ($number1<$number2) {
+        echo "<p>True. $number1<$number2</p>";
+    } else {
+        echo "<p>False. $number1<$number2</p>";
+    }
+
+    $number2 = 10;
+
+    if ($number1=$number2) {
+        echo "<p>True. $number1=$number2</p>";
+    } else {
+        echo "<p>False. $number1=$number2</p>";
+    }
+
+    if ($number1>=$number2) {
+        echo "<p>True. $number1>=$number2</p>";
+    } else {
+        echo "<p>False. $number1>=$number2</p>";
+    }
+
+    if ($number1<=$number2) {
+        echo "<p>True. $number1<=$number2</p>";
+    } else {
+        echo "<p>False. $number1<=$number2</p>";
+    }
+
+    $number1 = 2;
+
+    if ($number1<=$number2) {
+        echo "<p>True. $number1<=$number2</p>";
+    } else {
+        echo "<p>False. $number1<=$number2</p>";
+    }
+
 ?>
