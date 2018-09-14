7dbe82e327efa915dc6b6cc3acd151333a8d7886
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-09-14 16:20:24 -0400

    Completed "arithmetic" assignment and added associated screen capture

---
+++ series/arithmetic/index.php
@@ -0,0 +1,36 @@
+<?php
+    $sum = 10 + 20;
+    echo "<p>$sum</p>";
+
+    $sum = 10 - 20;
+    echo "<p>$sum</p>";
+
+    $sum = 10 * 20;
+    echo "<p>$sum</p>";
+
+    $sum = 10 / 20;
+    echo "<p>$sum</p>";
+
+    $number1 = 100;
+    $number2 = 50;
+    $number3 = 2;
+
+    $result = $number1 + $number2 / $number3;
+    echo "<p>$result</p>";
+    $result = ($number1 + $number2) / $number3;
+    echo "<p>$result</p>";
+
+    $number1 = 10;
+    $number2 = 3;
+
+    $result = $number1%$number2;
+    echo "<p>$result</p>";
+
+    $number1++;
+    echo "<p>$number1</p>";
+
+    $number1 = 10;
+
+    $number1 = $number1 + 1;
+    echo "<p>$number1</p>";
+?>
