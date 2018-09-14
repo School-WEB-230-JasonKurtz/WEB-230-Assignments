4a055e33902d6bf6af102c0e5dc066934c93f5ef
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-09-13 20:02:32 -0400

    Completed "operators" assignment and added associated screen capture

---
+++ series/operators/index.php
@@ -0,0 +1,24 @@
+<?php
+    $number1 = 10;
+
+    $number1 += 2;
+    echo "<p>$number1</p>";
+
+    $text = 'Hello';
+    $text .= ' World';
+    echo "<p>$text</p>";
+
+    //The following four code blocks are examples of shorthand assignment operators that where shown in the video.
+
+    $number1 -= 2;
+    echo "<p>$number1</p>";
+
+    $number1 *= 2;
+    echo "<p>$number1</p>";
+
+    $number1 /= 2;
+    echo "<p>$number1</p>";
+
+    $number1 %= 3;
+    echo "<p>$number1</p>";
+?>
