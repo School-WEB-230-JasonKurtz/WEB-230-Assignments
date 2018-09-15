9d488e5207cf62ad293c7be42b217db5f149b905
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-09-15 05:04:36 -0400

    Completed "triple-equals-operator" assignment and added associated screen capture

---
+++ series/triple-equals-operator/index.php
@@ -0,0 +1,16 @@
+<?php
+    $num1 = '1';
+    $num2 = 1;
+
+    if ($num1==$num2) {
+        echo '<p>'.'Equal.'.'</p>';
+    } else {
+        echo 'Not equal.';
+    }
+
+    if ($num1===$num2) {
+        echo 'Equal.';
+    } else {
+        echo '<p>'.'Not equal.'.'</p>';
+    }
+?>
