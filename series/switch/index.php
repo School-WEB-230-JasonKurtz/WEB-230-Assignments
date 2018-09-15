aaf32909d3d80ef4ac2481ce1aaf62cdfbcde16f
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-09-15 10:21:46 -0400

    Completed "switch" assignment and added associated screen capture

---
+++ series/switch/index.php
@@ -0,0 +1,130 @@
+<?php
+    $number = 1;
+
+    switch ($number) {
+        case 1:
+            echo 'One<br>';
+        break;
+
+        case 2:
+            echo 'Two<br>';
+        break;
+
+        case 3:
+            echo 'Three<br>';
+        break;
+    }
+
+    $number = 2;
+
+    switch ($number) {
+        case 1:
+            echo 'One<br>';
+        break;
+
+        case 2:
+            echo 'Two<br>';
+        break;
+
+        case 3:
+            echo 'Three<br>';
+        break;
+    }
+
+    $number = 3;
+
+    switch ($number) {
+        case 1:
+            echo 'One<br>';
+        break;
+
+        case 2:
+            echo 'Two<br>';
+        break;
+
+        case 3:
+            echo 'Three<br>';
+        break;
+    }
+
+    $number = 4;
+
+    switch ($number) {
+        case 1:
+            echo 'One<br>';
+        break;
+
+        case 2:
+            echo 'Two<br>';
+        break;
+
+        case 3:
+            echo 'Three<br>';
+        break;
+    }
+
+    $number = 4;
+
+    switch ($number) {
+        case 1:
+            echo 'One<br>';
+        break;
+
+        case 2:
+            echo 'Two<br>';
+        break;
+
+        case 3:
+            echo 'Three<br>';
+        break;
+
+        default:
+            echo 'Number not found.<br>';
+    }
+
+    $number = -10;
+
+    switch ($number) {
+        case 1:
+            echo 'One<br>';
+        break;
+
+        case 2:
+            echo 'Two<br>';
+        break;
+
+        case 3:
+            echo 'Three<br>';
+        break;
+
+        default:
+            echo 'Number not found.<br>';
+    }
+
+    $day = 'Monday';
+
+    switch ($day) {
+        case 'Saturday':
+        case 'Sunday':
+            echo 'It\'s a weekend.<br>';
+            break;
+
+        default:
+            echo 'Not a weekend.<br>';
+            break;
+    }
+
+    $day = 'Saturday';
+
+    switch ($day) {
+        case 'Saturday':
+        case 'Sunday':
+            echo 'It\'s a weekend.<br>';
+            break;
+
+        default:
+            echo 'Not a weekend.<br>';
+            break;
+    }
+
+?>
