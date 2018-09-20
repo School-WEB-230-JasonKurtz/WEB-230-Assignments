71ebb70cab19f7306162dd5af59ef157e5068945
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-09-20 16:51:16 -0400

    Completed "functions-with-return" assignment and added associated screen capture

---
+++ series/functions-with-return/index.php
@@ -0,0 +1,18 @@
+<?php
+	function add($number1, $number2) {
+		$result = $number1 + $number2;
+		return $result;
+	}
+
+	echo add(10,10).'<br>';
+	echo add(10,10) + 100 .'<br>';
+
+	function divide($number1, $number2) {
+		$result = $number1 / $number2;
+		return $result;
+	}
+
+	$sum = divide(add(10,10), add(5,5));
+
+	echo $sum;
+?>
