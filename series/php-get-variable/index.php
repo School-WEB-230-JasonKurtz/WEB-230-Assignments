b7e63add9de3763f5dd47c0983bc2730b65e3a31
Brian Savage <brianrsavage@users.noreply.github.com>
2018-11-05 11:33:24 -0500

    Update index.php

--- series/php-get-variable/index.php
+++ series/php-get-variable/index.php
@@ -1,6 +1,11 @@
 <?php
 
-	// 01 The first if checks to see if the user has hit the subit button. If not it doesn't display anything. If the user has has submitted information, the second if desides if all three feilds have information submitted. If the user left one or more feilds blank, the else statement tells the user to fill in all fields. If all fields are filled in it outputs the information entered.
+	/* 
+		01 The first if checks to see if the user has hit the subit button. If not it doesn't display anything. 
+		If the user has has submitted information, the second if desides if all three feilds have information submitted. 
+		If the user left one or more feilds blank, the else statement tells the user to fill in all fields. If all 
+		fields are filled in it outputs the information entered.
+	*/
 	if (isset($_GET['day'])&&isset($_GET['date'])&&isset($_GET['year'])) {
 		$day = $_GET['day'];
 		$date = $_GET['date'];
