d1baf77e785259ea0c5c7f8e1a28ffadd8603e37
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-11-18 20:18:05 -0500

    Completed "create-Create Sub-domain for WEB-230" assignment

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Web 230 Assignments</title>
+	</head>
+	<body>
+		<h1>Web 230 Assignments</h2>
+	</body>
+</html>
